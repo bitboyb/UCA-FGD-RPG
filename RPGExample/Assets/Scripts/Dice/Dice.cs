76dfe275ab32bc410089d4573e8c2c9fcffe88ac
Syed Assad Abbas Bokhari <62619775+bitboyb@users.noreply.github.com>
2024-10-04 12:52:58 +0100

    added comments

--- RPGExample/Assets/Scripts/Dice/Dice.cs
+++ RPGExample/Assets/Scripts/Dice/Dice.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 namespace Dice
 {
+    // the "[Serializable]" macro tells unity that this class can be used in the inspector like other data types
     [Serializable]
     public class Dice
     {
+        // this is declaring a data type, in this case, an integer which is a whole number like 0 , 10 , 256
         public int sides;
 
+        // This is a method/function which rolls this specific dice!
         public void Roll()
         {
+            // "Debug" is a class found in the UnityEngine namespace which is used to print to console
+            // ".Log" is a method/function found in the class to print the information in the ()
             Debug.Log("Dice " + sides + " :: " + UnityEngine.Random.Range(1, sides + 1));
         }
     }
--- RPGExample/Assets/Scripts/Dice/DiceManager.cs
+++ RPGExample/Assets/Scripts/Dice/DiceManager.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this makes the class a part of the Dice namespace, this is just a way to organise our code and we can use the "using" keyword to use theses namespaces.
 namespace Dice
 {
     public class DiceManager : MonoBehaviour
     {
+        // this is declaring a List, which is like a bucket we can carry multiples of the same time. So it is like a bucket full of dice!
+        // the "new List<Dice>()" just means we are creating a new list when this object comes into existence, it will delete anything done in the inspector.
+        public List<Dice> dice = new List<Dice>();
+
+        // this is a method/function, it is called when the object comes into existence.
+        // as you can see, we are calling/running our custom methods!
         public void Start()
         {
             CreateDice(6);
@@ -14,14 +21,19 @@ namespace Dice
             RollAllDice();
         }
 
-        public List<Dice> dice = new List<Dice>();
+        // this method creates dice and take a parameter to change the sides it has
         public void CreateDice(int sides)
         {
+            // this is a local variable, it only exists inside this method
+            // it is creating a new dice
             var newDice = new Dice();
+            // this is changing the amount of sides on the dice
             newDice.sides = sides;
+            // this is adding our dice to our list (like dropping the dice in the bucket once we have made it)
             dice.Add(newDice);
         }
 
+        // this rolls all the dice in our bucket
         public void RollAllDice()
         {
             foreach (Dice d in dice) d.Roll();
